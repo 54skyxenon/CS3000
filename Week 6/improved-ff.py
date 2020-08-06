72aa512220401953cf538d1eeceee371473f5de2
Brandon Liang <54skyxenon@gmail.com>
2020-08-05 21:11:53 -0700

    added fattest path to improved ford fulkerson

--- Week 6/improved-ff.py
+++ Week 6/improved-ff.py
@@ -1,7 +1,40 @@
 #!/usr/local/bin/python3
 # Author: Brandon Liang
 
+# so we can use heapdict
+import sys
+import os
+sys.path.append(os.path.abspath('../Week 4'))
+
 from collections import defaultdict
+from heapdict import maxHeapdict
+
+# DIFFERENCE: We'll select a smart augmented path this time
+# Two methods: fattest path and shortest path
+
+def fattestPath(network, start, end):
+    # Let Q be a new max heap
+    Q = maxHeapdict((u, float('-inf') if u != start else float('inf')) for u in network)
+    lookup = maxHeapdict((u, float('-inf') if u != start else float('inf')) for u in network)
+    parent = dict((v, None) for v in network)
+        
+    while Q:
+        # pop the largest item
+        u, width = Q.popitem()
+        for v in network[u]:
+            alt = max(lookup[v], min(lookup[u], network[u][v]))
+            if alt > lookup[v]:
+                Q[v] = alt
+                lookup[v] = alt
+                parent[v] = u
+                    
+    path = [end]
+    while parent[path[0]]:
+        path = [parent[path[0]]] + path
+    return path
+
+def shortestPath(network, start, end):
+    pass
 
 def fordFulkerson(network):
     residual = defaultdict(dict)
@@ -10,22 +43,30 @@ def fordFulkerson(network):
             residual[u][v] = network[u][v]
             residual[v][u] = 0
 
-    # DIFFERENCE: We'll select a smart augmented path this time
-    def findAugmentingPath(curr, acc, visited):
-        pass
+    # get rid of all zero edges on residual graph, so Dijkstra's doesn't die
+    def dropZeroEdges(network):
+        result = dict((v, dict()) for v in network)
+
+        for u in network:
+            for v in network[u]:
+                if network[u][v] > 0:
+                    result[u][v] = network[u][v]
+
+        return result
 
     maxFlow = float('-inf')
     while True:
-        augmentingPath = findAugmentingPath('s', ['s'], {'s'})
-
-        if not augmentingPath:
-            return maxFlow
+        augmentingPath = fattestPath(dropZeroEdges(residual), 's', 't')
 
         augment = float('inf')
         for i in range(len(augmentingPath) - 1):
             first = augmentingPath[i]
             second = augmentingPath[i + 1]
             augment = min(augment, residual[first][second])
+
+        # no more augmenting paths
+        if augment == float('inf'):
+            return maxFlow
 
         for i in range(len(augmentingPath) - 1):
             first = augmentingPath[i]
@@ -62,5 +103,3 @@ networkExample3['t'] = set()
 print('Max flow 1: {}'.format(fordFulkerson(networkExample1)))
 print('Max flow 2: {}'.format(fordFulkerson(networkExample2)))
 print('Max flow 3: {}'.format(fordFulkerson(networkExample3)))
-
-
